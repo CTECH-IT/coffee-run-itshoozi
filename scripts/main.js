9f2abfd0a915e2110699dc102b873ea02511a76b
itshoozi <jeremy@pringlecomputer.com>
2022-03-18 10:57:12 -0500

    Went back and finished Unit 4 Part 1, messed it up the first time

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-// Put your JavaScript here
+(function (window){
+    'use strict';
+
+    let App = window.App;
+    let Truck = App.Truck;
+    let DataStore = App.DataStore;
+
+    let myTruck = new App.Truck('12345', new App.DataStore());
+
+    window.myTruck = myTruck;
+    
+})(window);
